56b005c60cb5687aa758ae857197169822e7cdbe
studiominti <studiominti@gmail.com>
2020-02-27 01:46:28 -0300

    sessions controller, session routes, login helpers and new navbar condition

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,20 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'User was successfully created.'
+    else
+      render 'new', notice: 'User was successfully created.'
+    end
   end
 
   def show
   end
 
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :email, :password)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
+  #Sessions routes
+  resources :sessions, only: [:new, :create, :destroy]
+
+  #Users routes
+  resources :users, only: [:index, :new, :create, :show]
+
+  #Storiesroutes
   resources :stories
   root 'stories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
