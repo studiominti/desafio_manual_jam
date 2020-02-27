da3f230ace5d54c879854a796e46ab4f28e20b17
studiominti <studiominti@gmail.com>
2020-02-27 02:03:49 -0300

    login routes fix and session destroy method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,13 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
   end
 
   def destroy
+    reset_session
+    redirect_to root_path, notice: 'Signed out successfully'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   #Sessions routes
   resources :sessions, only: [:new, :create, :destroy]
+  get 'users/sign_up', to: "users#new", as: 'users'
+  post 'users/sign_up', to: "users#create"
 
   #Users routes
-  resources :users, only: [:index, :new, :create, :show]
+  resources :users, only: [:index, :new, :create]
+  get 'users/show'
+  delete 'users/logout', to: 'sessions#destroy', as: 'destroy_user_session'
 
   #Storiesroutes
   resources :stories
